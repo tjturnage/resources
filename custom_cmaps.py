fbe35310230e610d63fb4b8382d48e2217c65989
T.J <thomas.turnage@noaa.gov>
2019-10-19 09:19:56 -0400

    Add files via upload

--- custom_cmaps.py
+++ custom_cmaps.py
@@ -66,6 +66,8 @@ import matplotlib.pyplot as plt
 import sys
 from metpy.plots import colortables
 
+plts = {}
+
 #-------- Begin creating custom color maps --------
 
 #--- Reflectivity
@@ -73,17 +75,34 @@ ref_colors = [(0,0,0),(130,130,130),(95,189,207),(57,201,105),(57,201,105),(0,40
 ref_position = [0, 45/110, 46/110, 50/110, 51/110, 65/110, 66/110, 70/110, 71/110, 80/110, 81/110, 90/110, 91/110, 100/110, 101/110, 105/110, 106/110, 107/110, 109/110, 1]
 ref_cmap=make_cmap(ref_colors, position=ref_position,bit=True)
 plt.register_cmap(cmap=ref_cmap)
+plts['ReflectivityQC'] = {'cmap':ref_cmap,'vmn':-30,'vmx':80,'title':'Reflectivity','cbticks':[0,15,30,50,60],'cblabel':'dBZ'}
+plts['Ref'] = {'cmap':ref_cmap,'vmn':-30,'vmx':80,'title':'Reflectivity','cbticks':[0,15,30,50,60],'cblabel':'dBZ','title':'Reflectivity'}
 
 #--- Velocity - need to home grow this so I don't require import from metpy
 v_norm, v_cmap = colortables.get_with_range('NWS8bitVel', -40, 40)
 v_cmap.set_under('k')
 plt.register_cmap(cmap=v_cmap)
+plts['Velocity'] = {'cmap':v_cmap,'vmn':-100,'vmx':100,'title':'Velocity','cbticks':[-100,-80,-60,-40,-20,0,20,40,60,80,100],'cblabel':'kts'}
+plts['Vel'] = {'cmap':v_cmap,'vmn':-100,'vmx':100,'title':'Velocity','cbticks':[-100,-80,-60,-40,-20,0,20,40,60,80,100],'cblabel':'kts'}
+plts['SRV'] = {'cmap':v_cmap,'vmn':-100,'vmx':100,'title':'SRV','cbticks':[-100,-80,-60,-40,-20,0,20,40,60,80,100],'cblabel':'kts'}
 
 #--- Spectrum Width
 sw_colors = [(0,0,0),(220,220,255),(180,180,240),(50,50,150),(255,255,0),(255,150,0),(255,0,0),(255,255,255)]
 sw_position = [0, 1/40, 5/40, 0.25, 15/40, 0.5, 0.75, 1]
 sw_cmap=make_cmap(sw_colors, position=sw_position,bit=True)
 plt.register_cmap(cmap=sw_cmap)
+plts['SpectrumWidth'] = {'cmap':sw_cmap,'vmn':0,'vmx':40,'title':'Spectrum Width','cbticks':[0,10,15,20,25,40],'cblabel':'kts'}
+
+#--- CC
+#cc_colors = [(145,0,135),(80,0,0),(250,0,0),(255,110,0),(255,210,0),(255,255,0),(130,230,0),(85,255,85),(125,125,255),(0,0,255),(20,20,90),(140,140,140),(50,50,50),(50,50,50),(255,165,255),(255,165,255)]
+#cc_colors = [(20,0,50),(30,0,80),(0,0,110),(0,0,170),(0,0,255),(125,125,255),(85,255,85),(130,230,0),(255,255,0),(255,210,0),(255,110,0),(255,0,0),(80,0,0),(25,25,25),(170,170,170),(170,170,170)]
+#cc_colors = [(20,0,50),(30,0,80),(0,0,110),(0,0,170),(0,0,255),(125,125,255),(85,255,85),(130,230,0),(255,255,0),(255,210,0),(255,110,0),(255,0,0),(80,0,0),(145,0,135),(255,165,255),(255,165,255)]
+#cc_position = [0, 25/84, 35/84, 50/84, 55/84, 60/84, 65/84, 69/84, 70/84, 71.5/84, 74/84, 76/84, 78/84, 80/84, 80.01/84, 1 ]
+cc_colors = [(175,175,175),(255,225,200),(100,50,50),(175,150,120),(255,255,75),(35,100,35),(100,255,100),(175,175,175)]
+cc_position = [0, 10/105, 50/105, 70/105, 90/105, 96/105, 100/105, 1]
+cc_cmap=make_cmap(cc_colors, position=cc_position,bit=True)
+plt.register_cmap(cmap=cc_cmap)
+plts['RhoHV'] = {'cmap':cc_cmap,'vmn':0.00,'vmx':1.05,'title':'Correlation Coefficient','cbticks':[0.4,0.6,0.8,0.9,1.0],'cblabel':' '}
 
 #--- Velocity Gradient
 vg_colors = [(0, 0, 0),(32,32,32),(128,128,128),(117,70,0),(151,70,0),(186,70,0),(220,132,0),(255,153,0),(119,0,0),(153,0,0),(187,0,0),
@@ -91,6 +110,8 @@ vg_colors = [(0, 0, 0),(32,32,32),(128,128,128),(117,70,0),(151,70,0),(186,70,0)
 vg_position = [0, 1/15, 2/15, 3/15, 4/15, 5/15, 6/15, 7/15, 8/15, 9/15, 10/15, 11/15, 12/15, 13/15, 14/15, 0.999999, 1 ]
 vg_cmap=make_cmap(vg_colors, position=vg_position,bit=True)
 plt.register_cmap(cmap=vg_cmap)
+plts['Velocity_Gradient_Storm'] = {'cmap':vg_cmap,'vmn':0.000,'vmx':0.015,'title':'Velocity Gradient','cbticks':[0,0.005,0.010,0.015],'cblabel':'s $\mathregular{^-}{^1}$'}
+
 
 #--- Azimuthal Shear / Div Shear
 azdv_colors = [(1,1,1),(1,1,1),(0,0,1),(0,0,0.7),(0,0,0),(0.7,0,0),(1,0,0),(1,1,1),(1,1,1)]
@@ -105,6 +126,9 @@ azdv_position_r = [0, 0.001, 0.3, 0.43, 0.5, 0.57, 0.7, 0.999, 1]
 azdv_cmap_r=make_cmap(azdv_colors_r, position=azdv_position_r)
 plt.register_cmap(cmap=azdv_cmap_r)
 
+plts['DivShear_Storm'] = {'cmap':azdv_cmap_r,'vmn':-0.01,'vmx':0.01,'title':'DivShear','cbticks':[-0.010,-0.005,0,0.005,0.010],'cblabel':'s $\mathregular{^-}{^1}$'}
+plts['AzShear_Storm'] = {'cmap':azdv_cmap,'vmn':-0.01,'vmx':0.01,'title':'AzShear','cbticks':[-0.010,-0.005,0,0.005,0.010],'cblabel':'s $\mathregular{^-}{^1}$'}
+
 #--------------   Satellite
 # got a lot of help from - http://almanydesigns.com/grx/goes/
 
@@ -114,12 +138,17 @@ ir_colors = [(0,0,0),(255,255,255),(0,0,0), (255,0,0), (255,255,0), (0,255,0), (
 ir_position = [0, 10/166, 35/166, 45/166, 55/166, 65/166, 82/166, 90/166, 95/166, 135.9/166, 136/166, 1]
 ir_cmap=make_cmap(ir_colors, position=ir_position,bit=True)
 plt.register_cmap(cmap=ir_cmap)
+plts['C13'] = {'cmap':ir_cmap,'vmn':-110.0,'vmx':56.0,'title':'Channel 13 IR'}
 
 #--- W/V CH08,09,10
 wv_colors = [(0,255,255),(0,110,0),(255,255,255),(0,0,165),(255,255,0),(255,0,0),(0, 0, 0)]
 wv_position = [0, (109-75)/109,(109-47)/109, (109-30)/109, (109-15.5)/109,108/109,1 ]
 wv_cmap=make_cmap(wv_colors, position=wv_position,bit=True)
 plt.register_cmap(cmap=wv_cmap)
+plts['C08'] = {'cmap':wv_cmap,'vmn':-109.0,'vmx':0.0,'title':'Channel 8 W/V'}
+plts['C09'] = {'cmap':wv_cmap,'vmn':-109.0,'vmx':0.0,'title':'Channel 9 W/V'}
+plts['C10'] = {'cmap':wv_cmap,'vmn':-109.0,'vmx':0.0,'title':'Channel 10 W/V'}
+
 
 #--- Lightning
 ltg_colors = [(4/5,0,0),(0,4/5,0),(0,0,4/5)]
@@ -129,32 +158,18 @@ plt.register_cmap(cmap=ltg_cmap)
 
 #-------- End creating custom color maps --------
 
-plts = {}
+
 #plts['C02'] = {'cmap':'Greys_r','vmn':0.0,'vmx':1.0,'title':'Channel 2 Visible'}
 plts['C02i'] = {'cmap':'Greys_r','vmn':0.0,'vmx':1.0,'title':'Channel 2 Visible'}
 plts['C02'] = {'cmap':'Greys_r','vmn':0.0,'vmx':1.0,'title':'Channel 2 Visible'}
 plts['C03'] = {'cmap':'Greys_r','vmn':0.0,'vmx':1.0,'title':'Channel 3 Near IR'}
-plts['C08'] = {'cmap':wv_cmap,'vmn':-109.0,'vmx':0.0,'title':'Channel 8 W/V'}
-plts['C09'] = {'cmap':wv_cmap,'vmn':-109.0,'vmx':0.0,'title':'Channel 9 W/V'}
-plts['C10'] = {'cmap':wv_cmap,'vmn':-109.0,'vmx':0.0,'title':'Channel 10 W/V'}
-plts['C13'] = {'cmap':ir_cmap,'vmn':-110.0,'vmx':56.0,'title':'Channel 13 IR'}
+
+
 plts['GLM'] = {'title':'GLM'}
 plts['ltg_low'] = {'title':'Low Lightning'}
 plts['ltg_high'] = {'title':'High Lightning'}
-plts['Ref'] = {'cmap':ref_cmap,'vmn':-30,'vmx':80,'title':'Reflectivity','cbticks':[0,15,30,50,60],'cblabel':'dBZ','title':'Reflectivity'}
-plts['Vel'] = {'cmap':v_cmap,'vmn':-100,'vmx':100,'title':'Velocity','cbticks':[-100,-80,-60,-40,-20,0,20,40,60,80,100],'cblabel':'kts'}
 
-
-
-plts['Velocity_Gradient_Storm'] = {'cmap':vg_cmap,'vmn':0.000,'vmx':0.015,'title':'Velocity Gradient','cbticks':[0,0.005,0.010,0.015],'cblabel':'s $\mathregular{^-}{^1}$'}
-plts['Conv_Shear_Gradient'] = {'cmap':vg_cmap,'vmn':0.000,'vmx':0.015,'title':'Conv Shear Gradient','cbticks':[0,0.005,0.010,0.015],'cblabel':'s $\mathregular{^-}{^1}$'}
-plts['DivShear_Storm'] = {'cmap':azdv_cmap_r,'vmn':-0.01,'vmx':0.01,'title':'DivShear','cbticks':[-0.010,-0.005,0,0.005,0.010],'cblabel':'s $\mathregular{^-}{^1}$'}
-plts['AzShear_Storm'] = {'cmap':azdv_cmap,'vmn':-0.01,'vmx':0.01,'title':'AzShear','cbticks':[-0.010,-0.005,0,0.005,0.010],'cblabel':'s $\mathregular{^-}{^1}$'}
-plts['AzShear_Pos'] = {'cmap':azdv_cmap,'vmn':-0.01,'vmx':0.01,'title':'Positive AzShear','cbticks':[-0.010,-0.005,0,0.005,0.010],'cblabel':'s $\mathregular{^-}{^1}$'}
-plts['Velocity'] = {'cmap':v_cmap,'vmn':-100,'vmx':100,'title':'Velocity','cbticks':[-100,-80,-60,-40,-20,0,20,40,60,80,100],'cblabel':'kts'}
-plts['SRV'] = {'cmap':v_cmap,'vmn':-100,'vmx':100,'title':'SRV','cbticks':[-100,-80,-60,-40,-20,0,20,40,60,80,100],'cblabel':'kts'}
-plts['SpectrumWidth'] = {'cmap':sw_cmap,'vmn':0,'vmx':40,'title':'Spectrum Width','cbticks':[0,10,15,20,25,40],'cblabel':'kts'}
-plts['ReflectivityQC'] = {'cmap':ref_cmap,'vmn':-30,'vmx':80,'title':'Reflectivity','cbticks':[0,15,30,50,60],'cblabel':'dBZ'}
-
+#plts['Conv_Shear_Gradient'] = {'cmap':vg_cmap,'vmn':0.000,'vmx':0.015,'title':'Conv Shear Gradient','cbticks':[0,0.005,0.010,0.015],'cblabel':'s $\mathregular{^-}{^1}$'}
+#plts['AzShear_Pos'] = {'cmap':azdv_cmap,'vmn':-0.01,'vmx':0.01,'title':'Positive AzShear','cbticks':[-0.010,-0.005,0,0.005,0.010],'cblabel':'s $\mathregular{^-}{^1}$'}
 #plts['DivShear_Neg'] = {'cmap':azdv_cmap_r,'vmn':0.000,'vmx':0.015,'title':'Convergent Shear','cbticks':[0,0.005,0.010,0.015],'cblabel':'s $\mathregular{^-}{^1}$'}
 #plts['AzShear_Pos'] = {'cmap':azdv_cmap,'vmn':-0.01,'vmx':0.01,'title':'Positive AzShear','cbticks':[-0.010,-0.005,0,0.005,0.010],'cblabel':'s $\mathregular{^-}{^1}$'}
